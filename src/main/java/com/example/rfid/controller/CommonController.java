3a97e0d757493e536886c47105dd03730dc8e1f0
“xiayudi” <xiayudi@meituan.com>
2021-04-13 00:28:01 +0800

    完善代码

--- src/main/java/com/example/rfid/controller/CommonController.java
+++ src/main/java/com/example/rfid/controller/CommonController.java
@@ -1,6 +1,7 @@
 package com.example.rfid.controller;
 
 import com.example.rfid.service.RfidReadService;
+import com.example.rfid.service.RfidService;
 import com.example.rfid.vo.InitVO;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Controller;
@@ -14,7 +15,8 @@ import javax.annotation.Resource;
 public class CommonController {
 	@Resource
 	private RfidReadService rfidReadService;
-
+	@Resource
+	private RfidService rfidService;
 	@GetMapping("/rfid/read")
 	public String rfidRead(Model model) {
 //		List<Method> methodList = methodService.findAll();
@@ -31,4 +33,33 @@ public class CommonController {
 		model.addAttribute("pageInfo", pageInfo);
 		return "rfid/rfid_read_list";
 	}
+
+	@GetMapping("/rfid/test")
+	public String index(Model model) {
+		//rfidService.readRfid();
+		return "rfid/rfid_test";
+	}
+
+	@GetMapping("/rfid/test2")
+	public String index2(Model model) {
+		rfidService.readRfid();
+		return "rfid/rfid_test";
+	}
+
+	@GetMapping("/rfid/test3")
+	public String index3(Model model) {
+		//rfidService.writeRfid("1111111111111111111111111111");
+		rfidService.writeRfid("2222222222222222222222222222222222222222222228");
+		//rfidService.writeRfid("1111111111111111111111111111111111111111111118");
+		//rfidService.writeRfid("3333333333333333333333333333");
+		return "rfid/rfid_test";
+	}
+
+	@GetMapping("/rfid/test4")
+	public String index4(Model model) {
+		rfidService.resetRfid();
+		return "rfid/rfid_test";
+	}
+
+
 }
--- src/main/java/com/example/rfid/rfidHelper/config/HEAD.java
+++
@@ -1,6 +0,0 @@
-package com.example.rfid.rfidHelper.config;
-
-public class HEAD {
-	/**UHF mark to indicate the head of data package */
-	public final static byte HEAD = (byte) 0xA0;
-}
